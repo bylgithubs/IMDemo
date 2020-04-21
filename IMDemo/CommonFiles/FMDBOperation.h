2c7fd49294eb3187b07d65854c54ebaa997d9df8
廖宝裕 <1576660896@qq.com>
2020-04-21 10:19:54 +0800

    新增FMDBOperation文件

---
+++ IMDemo/CommonFiles/FMDBOperation.h
@@ -0,0 +1,17 @@
+//
+//  FMDBOperation.h
+//  IMDemo
+//
+//  Created by Civet on 2020/4/21.
+//  Copyright © 2020 PersonalONBYL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FMDBOperation : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ IMDemo/CommonFiles/FMDBOperation.m
@@ -0,0 +1,13 @@
+//
+//  FMDBOperation.m
+//  IMDemo
+//
+//  Created by Civet on 2020/4/21.
+//  Copyright © 2020 PersonalONBYL. All rights reserved.
+//
+
+#import "FMDBOperation.h"
+
+@implementation FMDBOperation
+
+@end
